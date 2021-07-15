270ca340e6fdf6379a915cadf7fb8ae64e5ecc77
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2021-07-15 16:10:10 -0300

    adding react component in new places

--- screens/NewPlaceScreen.tsx
+++ screens/NewPlaceScreen.tsx
@@ -1,13 +1,50 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Button,
+  TextInput,
+  Text,
+  ScrollView,
+} from "react-native";
+import Color from "../constants/Color";
 
 const NewPlaceScreen = (props: any) => {
+  const [title, setTitle] = useState("");
+  const savePlaceHandler = () => {};
   return (
-    <View>
-      <Text> New Place</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}> New Place</Text>
+        <TextInput
+          onChangeText={(text) => setTitle(text)}
+          value={title}
+          style={styles.textInput}
+        />
+        <Button
+          title="Save Place"
+          color={Color.primary}
+          onPress={() => savePlaceHandler()}
+        />
+      </View>
+    </ScrollView>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+});
 
 export default NewPlaceScreen;
